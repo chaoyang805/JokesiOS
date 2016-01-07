528e39ddc1559f8be92a33c13bfed9c1b06699a5
chaoyang805 <zhangchaoyang805@gmail.com>
2016-01-07 11:24:15 +0800

    Initial Commit

---
+++ JokesiOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JokesiOS
+//
+//  Created by chaoyang805 on 16/1/7.
+//  Copyright © 2016年 com.jikexueyuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JokesiOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JokesiOS
+//
+//  Created by chaoyang805 on 16/1/7.
+//  Copyright © 2016年 com.jikexueyuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
